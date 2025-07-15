2e131feb87fae4379ce8dd2b53e497bc7377775b
Ridho Pujiono <ridhopujiono2@gmail.com>
2025-07-15 08:24:12 +0700

    feat(auth): Add authentication route and clustering detail view

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/auth', [AuthController::class, 'authenticate'])->name('auth');
+
 Route::get('/dashboard', function () {
     return view('dashboard.dashboard.index');
 })->name('dashboard');
 Route::get('/clustering', function () {
     return view('dashboard.clustering.index');
 })->name('clustering');
+Route::get('/clustering/detail', function () {
+    return view('dashboard.clustering.detail');
+})->name('clustering.detail');
 Route::get('/screening', function () {
     return view('dashboard.screening.index');
 })->name('screening');
