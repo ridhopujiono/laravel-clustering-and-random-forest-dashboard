ecce7024f5668edef9778a20c75125185a28456d
Ridho Pujiono <ridhopujiono2@gmail.com>
2025-07-27 12:23:14 +0700

    chore(footer): Update copyright year to 2025

--- resources/views/dashboard/template/footer.blade.php
+++ resources/views/dashboard/template/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
-            <p>2024 &copy; Platform Stunting</p>
+            <p>2025 &copy; Platform Stunting</p>
         </div>
         <div class="float-end">
             <p>Dibuat dengan <span class="text-danger"><i class="bi bi-heart-fill"></i></span> oleh Tim Analitik</p>
